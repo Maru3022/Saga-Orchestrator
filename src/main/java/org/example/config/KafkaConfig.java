865e3673f805e3fef4389568d7e3c43eac2d5ee7
Vitali Artyomchik <pdijsj2992@gmail.com>
2026-05-01 14:51:26 +0300

    feat: set up Kafka and Redis configurations and main application entry point

--- src/main/java/org/example/config/KafkaConfig.java
+++ src/main/java/org/example/config/KafkaConfig.java
@@ -1,4 +1,40 @@
 package org.example.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaConfig {
+
+    @Bean
+    public NewTopic trainsCommandTopic() {
+        return TopicBuilder.name("saga-trains-command").partitions(3).replicas(1).build();
+    }
+
+    @Bean
+    public NewTopic nutritionCommandTopic(){
+        return TopicBuilder.name("saga-nutrition-command").partitions(3).replicas(1).build();
+    }
+
+    @Bean
+    public NewTopic notificationCommandTopic(){
+        return TopicBuilder.name("saga-notification-command").partitions(3).replicas(1).build();
+    }
+
+    @Bean
+    public NewTopic trainsResponseTopic(){
+        return TopicBuilder.name("saga-trains-response").partitions(3).replicas(1).build();
+    }
+
+    @Bean
+    public NewTopic nutritionResponseTopic(){
+        return TopicBuilder.name("saga-nutrition-response").partitions(3).replicas(1).build();
+    }
+
+    @Bean
+    public NewTopic notificationResponseTopic(){
+        return TopicBuilder.name("saga-notification-response").partitions(3).replicas(1).build();
+    }
 }
