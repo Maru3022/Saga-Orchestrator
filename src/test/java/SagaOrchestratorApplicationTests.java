24de031b7c8e668d2d5b5b3517a556a29aefea13
Vitali Artyomchik <pdijsj2992@gmail.com>
2026-05-08 20:19:40 +0300

    chore: update multiple microservice configs and tests

--- src/test/java/SagaOrchestratorApplicationTests.java
+++ src/test/java/SagaOrchestratorApplicationTests.java
@@ -2,8 +2,15 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, topics = {
+        "saga-trains-command", "saga-nutrition-command", "saga-notification-command",
+        "saga-trains-response", "saga-nutrition-response", "saga-notification-response"
+})
 class SagaOrchestratorApplicationTests {
 
     @Test
--- src/test/java/config/KafkaConfigTest.java
+++ src/test/java/config/KafkaConfigTest.java
@@ -4,12 +4,19 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, topics = {
+        "saga-trains-command", "saga-nutrition-command", "saga-notification-command",
+        "saga-trains-response", "saga-nutrition-response", "saga-notification-response"
+})
 public class KafkaConfigTest {
 
     @Autowired
