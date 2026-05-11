c8286682589a1dae103c702d9409731a7e08f983
Vitali Artyomchik <pdijsj2992@gmail.com>
2026-05-11 20:07:58 +0300

    test(saga): add unit tests for step handlers and orchestrator
    added tests for NotificationStepHandler and NutritionStepHandler
    
    implemented SagaOrchestratorTest coverage
    
    updated pom.xml dependencies

--- src/test/java/service/handlers/NotificationStepHandlerTest.java
+++ src/test/java/service/handlers/NotificationStepHandlerTest.java
@@ -1,4 +1,46 @@
 package service.handlers;
 
+import jakarta.inject.Inject;
+import org.example.model.SagaEvent;
+import org.example.service.handlers.NotificationStepHandler;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class NotificationStepHandlerTest {
+
+    @Mock
+    private KafkaTemplate<String, SagaEvent> kafkaTemplate;
+
+    @InjectMocks
+    private NotificationStepHandler handler;
+
+    @Test
+    void shouldProcessForward(){
+        SagaEvent sagaEvent = new SagaEvent("saga-1","NOTIFICATION","EXECUTE",null);
+        handler.processForward(sagaEvent);
+        verify(kafkaTemplate).send(eq("saga-notification-command"),any(SagaEvent.class));
+    }
+
+    @Test
+    void shouldProcessRollBack(){
+        SagaEvent event = new SagaEvent("saga-2","NOTIFICATION","ROLLBACK","data");
+        handler.processRollback(event);
+        verify(kafkaTemplate).send(eq("saga-notification-command"),any(SagaEvent.class));
+    }
+
+    @Test
+    void shouldReturnStepName(){
+        assertThat(handler.getStepName()).isEqualTo("NOTIFICATION");
+    }
+
 }
--- src/test/java/service/handlers/NutritionStepHandlerTest.java
+++ src/test/java/service/handlers/NutritionStepHandlerTest.java
@@ -1,4 +1,48 @@
 package service.handlers;
 
+import org.example.model.SagaEvent;
+import org.example.service.handlers.NutritionStepHandler;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class NutritionStepHandlerTest {
+
+    @Mock
+    private KafkaTemplate<String, SagaEvent> kafkaTemplate;
+
+    @InjectMocks
+    private NutritionStepHandler nutritionStepHandler;
+
+    @Test
+    void shouldProcessForward(){
+        SagaEvent sagaEvent = new SagaEvent("saga-1", "NUTRITION", "EXECUTE",null);
+        nutritionStepHandler.processForward(sagaEvent);
+        verify(kafkaTemplate).send(eq("saga--nutrition-command"),any(SagaEvent.class));
+    }
+
+    @Test
+    void shouldProcessRollback(){
+        SagaEvent event = new SagaEvent("saga-2","NUTRITION", "ROLLBACK", "data");
+        nutritionStepHandler.processRollback(event);
+        ArgumentCaptor<SagaEvent> captor = ArgumentCaptor.forClass(SagaEvent.class);
+        verify(kafkaTemplate).send(eq("saga--nutrition-command"),captor.capture());
+        assertThat(captor.getValue().getStatus()).isEqualTo("ROLLBACK");
+    }
+
+    @Test
+    void shouldReturnStepName(){
+        assertThat(nutritionStepHandler.getStepName()).isEqualTo("NUTRITION");
+    }
+
 }
